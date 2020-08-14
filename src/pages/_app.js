5c967c05d9189f751bd38b6b814eff94e626ad3b
Esteban Campos <campos.esteban@gmail.com>
2020-08-15 02:43:24 +1000

    init with theme-ui next example

---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NextApp from "next/app";
+
+import Header from "../components/Header";
+import theme from "../theme";
+import { ThemeProvider } from "theme-ui";
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
