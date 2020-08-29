36f0baab4da66943ed73b5523c094d1f74986a05
Esteban Campos <campos.esteban@gmail.com>
2020-08-29 18:33:13 +1000

    updating website.mdx

--- components/Layout/Header/Nav.js
+++ components/Layout/Header/Nav.js
@@ -44,7 +44,7 @@ const Nav = () => {
     window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.addEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", handleScroll);
       observer.disconnect();
     };
   }, []);
